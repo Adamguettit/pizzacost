c23b64abae9f50af3fc0c5bd70dbb13d26b65fb0
Adamguettit <36240633+Adamguettit@users.noreply.github.com>
2018-03-01 22:13:37 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@ subtotalofpizzaTextField.id = "subtotal textField"
 local taxTextField = display.newText("tax", display.contentCenterX, display.contentCenterY + 300, native.systemFont, 75)
 taxTextField.id = "tax textField"
 
+local totalTextFeild =display.newText("total", display.contentCenterX, display.contentCenterY + 400, native.systemFont, 75)
+
 local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 406, 157 )
 calculateButton.x = display.contentCenterX
 calculateButton.y = display.contentCenterY
@@ -26,14 +28,16 @@ local function calculateButtonTouch( event )
     local diameterofpizza
     local subtotalofpizza
     local tax
+    local total
  	
  	diameterofpizza = diameterofpizzaTextField.text
     subtotalofpizza = 0.75 + 1 + diameterofpizza * 0.5
     tax = subtotalofpizza * 0.13
-    -- print( areaOfSquare )
+    total = tax + subtotalofpizza
+    -- print( subtotal, tax, and total )
     subtotalofpizzaTextField.text = "subtotal is " .. subtotalofpizza
     taxTextField.text = "tax is " .. tax
-
+    totalTextFeild.text = "total is " ..total
     return true
 end
 
